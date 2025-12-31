d5320ffb51f1475dd36cb17254ba444b85df9bc2
ikedaShuya <k024g0046@g.neec.ac.jp>
2025-12-31 17:10:15 +0900

    外部ファイルから背景物の配置テーブルを読み込んだ

--- DirectXGame/GameScene.cpp
+++ DirectXGame/GameScene.cpp
@@ -1,15 +1,92 @@
 #include "GameScene.h"
+#include "Math.h"
 
 using namespace KamataEngine;
 
-void GameScene::Initialize() {}
+void GameScene::Initialize() {
+
+	// カメラの初期化
+	camera_.Initialize();
+
+	// ブロックモデルデータの生成
+	modelBlock_ = Model::CreateFromOBJ("cube", true);
+
+	mapChipField_ = new MapChipField;
+	mapChipField_->LoadMapChipCsv("Resources/blocks.csv");
+
+	GenerateBlocks();
+
+}
 
 void GameScene::Update() {
 
-	if (Input::GetInstance()->PushKey(DIK_RETURN)) {
+	if (Input::GetInstance()->TriggerKey(DIK_RETURN)) {
 		finished_ = true;
+	}
+
+	// ブロックの更新
+	for (std::vector<WorldTransform*>& worldTransformBlcokLine : worldTransformBlocks_) {
+		for (WorldTransform* worldTransformBlock : worldTransformBlcokLine) {
+			if (!worldTransformBlock)
+				continue;
+
+			WorldTransformUpdate(*worldTransformBlock);
+		}
 	}
 
 }
 
-void GameScene::Draw() {}
+void GameScene::Draw() {
+
+	// 3Dモデル描画前処理
+	Model::PreDraw();
+
+	// ブロックの描画
+	for (std::vector<WorldTransform*>& worldTransformBlcokLine : worldTransformBlocks_) {
+		for (WorldTransform* worldTransformBlock : worldTransformBlcokLine) {
+			if (!worldTransformBlock)
+				continue;
+
+			modelBlock_->Draw(*worldTransformBlock, camera_);
+		}
+	}
+
+	// 3Dモデル描画後処理
+	Model::PostDraw();
+
+}
+
+GameScene::~GameScene() {
+
+	// マップチップフィールドの解放
+	delete mapChipField_;
+}
+
+void GameScene::GenerateBlocks() {
+
+	// 要素数
+	uint32_t numBlockVirtical = mapChipField_->GetNumBlockVirtical();
+	uint32_t numBlockHorizontal = mapChipField_->GetNumBlockHorizontal();
+
+	// 要素数を変更する
+	// 列数を設定（縦方向のブロック数)
+	worldTransformBlocks_.resize(numBlockVirtical);
+	for (uint32_t i = 0; i < numBlockVirtical; ++i) {
+		// 1列の要素数を設定（横方向のブロック数)
+		worldTransformBlocks_[i].resize(numBlockHorizontal);
+	}
+
+	// ブロックの生成
+	// キューブの生成
+	for (uint32_t i = 0; i < numBlockVirtical; ++i) {
+		for (uint32_t j = 0; j < numBlockHorizontal; ++j) {
+			if (mapChipField_->GetMapChipTypeByIndex(j, i) == MapChipType::kBlock) {
+
+				WorldTransform* worldTransform = new WorldTransform();
+				worldTransform->Initialize();
+				worldTransformBlocks_[i][j] = worldTransform;
+				worldTransformBlocks_[i][j]->translation_ = mapChipField_->GetMapChipPositionByIndex(j, i);
+			}
+		}
+	}
+}
